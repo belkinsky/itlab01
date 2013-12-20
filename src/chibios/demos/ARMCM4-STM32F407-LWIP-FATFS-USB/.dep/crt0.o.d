4f3552aa43695a4583c154e8f728f748f118b8a4
belkinsky <belkinsky@gmail.com>
2013-12-20 14:08:49 +0400

    Add ChibiOS-RT sources

---
+++ src/chibios/demos/ARMCM4-STM32F407-LWIP-FATFS-USB/.dep/crt0.o.d
@@ -0,0 +1,22 @@
+build/obj/crt0.o: ../../os/ports/GCC/ARMCMx/crt0.c \
+ c:\fbin\yagarto\bin\../lib/gcc/arm-none-eabi/4.7.4/include/stdint.h \
+ c:\fbin\yagarto\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/stdint.h \
+ c:\fbin\yagarto\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/_ansi.h \
+ c:\fbin\yagarto\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/newlib.h \
+ c:\fbin\yagarto\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/sys/config.h \
+ c:\fbin\yagarto\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/machine/ieeefp.h \
+ c:\fbin\yagarto\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/sys/features.h
+
+c:\fbin\yagarto\bin\../lib/gcc/arm-none-eabi/4.7.4/include/stdint.h:
+
+c:\fbin\yagarto\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/stdint.h:
+
+c:\fbin\yagarto\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/_ansi.h:
+
+c:\fbin\yagarto\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/newlib.h:
+
+c:\fbin\yagarto\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/sys/config.h:
+
+c:\fbin\yagarto\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/machine/ieeefp.h:
+
+c:\fbin\yagarto\bin\../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/sys/features.h:
